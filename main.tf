6913f5e40cf983f3426a2baae5434aa3f32015bd
Michalis Stratis <100694301+MichalisStratis@users.noreply.github.com>
2024-08-26 01:43:10 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,8 @@
-resource "aws_vpc" "example_vpc" {
-  cidr_block = "10.0.0.0/16"
+# main.tf
 
-  tags = {
-    Name = "example-vpc"
-  }
-}
+provider "local" {}
 
-resource "aws_subnet" "example_subnet" {
-  vpc_id                  = aws_vpc.example_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2a"
-
-  tags = {
-    Name = "example-subnet"
-  }
-}
-
-resource "aws_instance" "example_instance" {
-  ami                    = "ami-0c94855ba95c71c99" # This is an Ubuntu AMI ID for us-west-2
-  instance_type          = var.instance_type
-  subnet_id              = aws_subnet.example_subnet.id
-
-  tags = {
-    Name = "example-instance"
-  }
+resource "local_file" "example" {
+  content  = "This is a test file created by Terraform!"
+  filename = "${path.module}/example_file.txt"
 }
